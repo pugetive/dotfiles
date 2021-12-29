3a2a5aa0ab46847c8bf40f726f9213a932f5fca4
Todd Gehman <t.gehman@celmatix.com>
2021-12-29 09:18:46 -0500

    fix for missing tab completion

--- zsh/config.sh
+++ zsh/config.sh
@@ -39,6 +39,8 @@ source $ZSH/oh-my-zsh.sh
 
 # Customize to your needs...
 
+autoload -Uz compinit && compinit
+
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
   export EDITOR='nano'
